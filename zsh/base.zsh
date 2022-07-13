5423774b4c3a2592dcdf46778f46b119b58daecf
Stephen Corlett <scorlett817@gmail.com>
2022-07-12 17:03:12 -0700

    Fixes

--- zsh/base.zsh
+++ zsh/base.zsh
@@ -22,7 +22,7 @@ export ZSH="$HOME/.oh-my-zsh"
 # load a random theme each time oh-my-zsh is loaded, in which case,
 # to know which specific one was loaded, run: echo $RANDOM_THEME
 # See https://github.com/ohmyzsh/ohmyzsh/wiki/Themes
-ZSH_THEME="powerlevel10k"
+ZSH_THEME="powerlevel10k/powerlevel10k"
 
 # Set list of themes to pick from when loading at random
 # Setting this variable when ZSH_THEME=random will cause zsh to load
@@ -120,5 +120,4 @@ alias source-zsh="source ~/.zshrc"
 if [[ $(uname) = Darwin ]]; then
   include "$DOTFILES/zsh/nvm.zsh"
   include "$DOTFILES/zsh/thinkific.zsh"
-  include "$DOTFILES/zsh/fzf.zsh"
 fi
